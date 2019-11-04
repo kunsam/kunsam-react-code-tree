f4088f138e947f334ee4ec11a6e3ea8b3f9fba07
kunsam <kunsam624@icloud.com>
2019-11-04 15:20:34 +0800

    new version

--- src/extension.ts
+++ src/extension.ts
@@ -75,6 +75,33 @@ export function activate(context: vscode.ExtensionContext) {
       });
     }
   );
+  class ActionCompletitionProvider implements vscode.CompletionItemProvider {
+    provideCompletionItems(): vscode.ProviderResult<
+      vscode.CompletionItem[] | vscode.CompletionList
+    > {
+      return new Promise(resolve => {
+        const item = new vscode.CompletionItem(
+          "le_at_generate: " + 'AppAction',
+          vscode.CompletionItemKind.Class
+        );
+        item.detail = 'LeTote New Action'
+        item.insertText = ActionClassCoder.getActionClassSimpleByQueryString('');
+        resolve([ item ]);
+      });
+    }
+  }
+  context.subscriptions.push(
+    vscode.languages.registerCompletionItemProvider(
+      [
+        { scheme: "file", language: "typescript" },
+        { scheme: "file", language: "javascript" },
+        { scheme: "file", language: "javascriptreact" },
+        { scheme: "file", language: "typescriptreact" }
+      ],
+      new ActionCompletitionProvider(),
+      "leatg"
+    )
+  )
 
   new NodeFlowCommands(context);
   new KeybindingCommands(context);
